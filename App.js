fbe49046824d6b319666c836a87c8e7fd9b2fd4f
ah-james <11ahj11@gmail.com>
2021-04-23 18:29:21 -0400

    Added Input to Home Page

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Select Your Workout")
+  const [outputText, setOutputText] = useState("Create a New Workout")
+  const [workoutText] = useState("Name Your Workout")
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
+      <TextInput style={styles.input} placeholder={workoutText}></TextInput>
       <Button title="Create a New Workout"/>
       <Button title="Change Text" onPress={() => setOutputText('Which Workout Are You Doing Today?')}/>
       <StatusBar style="auto" />
@@ -20,5 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
