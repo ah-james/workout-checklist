eea54f0a695db454509ea50e0ba546372343d054
ah-james <11ahj11@gmail.com>
2021-04-24 21:24:39 -0400

    Handling Form State Changes

--- App.js
+++ App.js
@@ -5,12 +5,22 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   const [outputText, setOutputText] = useState("Create a New Workout")
   const [workoutText] = useState("Name Your Workout")
+  const [enteredWorkout, setEnteredWorkout] = useState('')
+
+  const handleWorkoutInput = (enteredText) => {
+    setEnteredWorkout(enteredText)
+  }
+
+  const addWorkout = () => {
+    console.log(enteredWorkout)
+  }
+
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder={workoutText} />
-        <Button style={styles.button} title="Create" />
+        <TextInput style={styles.input} placeholder={workoutText} onChangeText={handleWorkoutInput} value={enteredWorkout} />
+        <Button style={styles.button} title="Create" onPress={addWorkout} />
       </View>
       <Button title="Change Text" onPress={() => setOutputText('Which Workout Are You Doing Today?')} />
       <StatusBar style="auto" />
