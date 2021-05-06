2aa90225eb560b12c35cd6eeaa556218e6342988
ah-james <11ahj11@gmail.com>
2021-05-06 16:37:36 -0400

    Created WoirkoutsHome Container Component

---
+++ containers/WorkoutsHome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, Stylesheet } from 'react-native'
+
+const WorkoutsHome = props => {
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default WorkoutsHome
